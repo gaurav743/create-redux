e0872a516ca28fed69f802a98770e441a859dbed
unknown <gaurav_tk@yahoo.co.in>
2020-04-27 20:52:17 +0530

    Added reducer and dispatch method.

--- store.js
+++ store.js
@@ -1,3 +1,5 @@
+///////////////////////////REDUX Library Code - Start ////////////////////////////////////
+
 /*   
     Function to emulate Redux store. A Redux store has following feature
     1) state tree that stores complete state of store.
@@ -6,13 +8,13 @@
     4) Update state
 */
 
-function createStore {
+function createStore(todos){
     //state tree.
-    const state
+    let state
 
     //There can be multiple events/clients that want to be notified every time state of store changes. We store them in an array here. In this case this will be 
     //an array of listener functions.
-    const listeners = []
+    let listeners = []
 
     //Return current state.
     const getState = () => state
@@ -24,24 +26,59 @@ function createStore {
         return () => listeners = listeners.filter( l => l != listener )
     }
 
+    const dispatch = (action) => { //dispatch function is responsible for updating the state of the store. It takes action as the argument.
+        state = todos(state, action)
+        listeners.forEach((listener) => listener()) //Once, state is updated all listeners must be notified. In this case we are invoking
+                                                    //all listeners as they are functions.
+    }
+
     return {
         getState,
-        subscribe
+        subscribe,
+        dispatch
     }
 }
 
+///////////////////////////REDUX Library Code - End ////////////////////////////////////
+
+/////////////////////////User Invoked Code /////////////////////////////////////////////
+
 //Instantiate store
-const store = createStore()
+const store = createStore(todos)
 
 //Listner 1
 store.subscribe( () => {
     console.log("The new state is", store.getState())
 })
 
+store.dispatch({
+    type: 'ADD_TODO',
+    todo: {
+        id: 1,
+        name: 'Learn React',
+        completed: false
+    }
+})
 //Listener 2. unsubscribe will store function to unsubscribe as variable.
-const unsubscribe = store.subscribe( () => {
+const unsubscribe = store.subscribe( setTimeout (() => {
     console.log("The state is updated.....")
-}) 
+}, 100))
+
+
 
 //This will invoke the anonymous function and will remove listener 2 from the array of listeners from store.
 unsubscribe()
+
+
+//Reducer TODOS function. A reducer function takes a state and action that will modify the state and return modified state.
+//A reducer must be a pure function.
+function todos(state = [], action){
+    if(action.type === 'ADD_TODO'){
+        return state.concat(action.todo)
+    }
+
+    return state
+}
+
+/////////////////////////User Invoked Code /////////////////////////////////////////////
+
